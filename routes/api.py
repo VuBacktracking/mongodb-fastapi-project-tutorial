c07398a2b978f5fe42635a207e00f7038f84e812
Vu Phan <vhoang9999@gmail.com>
2024-08-05 13:16:21 +0700

    Update routes

--- routes/api.py
+++ routes/api.py
@@ -5,7 +5,9 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from fastapi import APIRouter
-from endpoints import users
+from endpoints import users, books, addresses  # Make sure these are correctly imported
 
 router = APIRouter()
+router.include_router(books.router)
 router.include_router(users.router)
+router.include_router(addresses.router)
